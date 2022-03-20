6f9f7e8ed3cb8632ca3704cf74ef61efa0144a4a
Nikolai Ochirov <dubdubman@ya.ru>
2022-03-20 22:24:29 +0300

    fix import/export error

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { genDiff } from '../src/gendiff.js';
+import genDiff from '../src/gendiff.js';
 
 const program = new Command();
 program
