915233a9ce58184547ecb17a8bf258b7748bfe9a
Nikolai Ochirov <dubdubman@ya.ru>
2022-04-10 13:18:50 +0300

    add tests for json and yaml

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,49 +7,60 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
-test('test plain JSON diff', () => {
+test('test JSON diff with stylish formatter', () => {
   const filename1 = getFixturePath('file1.json');
   const filename2 = getFixturePath('file2.json');
-  const filename3 = getFixturePath('file3.json');
-  const filename4 = getFixturePath('file4.json');
 
   const answer1 = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
-  const answer2 = `{
-    follow: false
-  - host: hexlet.io
-  + host: room.io
-    proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 500
-  + lang: en
-  + title: string
-}`;
-  const answer3 = `{
-  - follow: false
-  - host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + font: Tahoma Serif
-  + lang: en
-  + menu: hidden
-  + meta: off
-  + outline: false
-  + title: string
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`;
 
   expect(genDiff(filename1, filename2)).toEqual(answer1);
-  expect(genDiff(filename1, filename3)).toEqual(answer2);
-  expect(genDiff(filename1, filename4)).toEqual(answer3);
+
 });
 
-test('test plain YAML diff', () => {
+test('test YAML diff with stylish formatter', () => {
   const answer1 = `{
   - follow: false
     host: hexlet.io
