762553d43826b1deb48648673e92635504db42de
Nikolai Ochirov <dubdubman@ya.ru>
2022-05-13 23:22:18 +0300

    wip stylish linter fixes

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,59 +9,47 @@ const stylish = (diff) => {
     const bracketIndent = ' '.repeat(indentSize - spacesCount);
 
     const lines = currentValue.map((item) => {
-      let rows = '';
       const {
-        name, value, type, status,
+        name, value, type, status, children, previousValue, previousValueType,
       } = item;
 
-      if (status === 'unchanged') {
-        if (type === 'primitive') {
-          rows += `${currentIndent}${name}: ${value}`;
-        } else {
-          const { children } = item;
-          rows += `${currentIndent}${name}: ${iter(children, depth + 1)}`;
-        }
-      }
+      switch (status) {
+        case 'unchanged':
+          return type === 'primitive'
+            ? `${currentIndent}${name}: ${value}`
+            : `${currentIndent}${name}: ${iter(children, depth + 1)}`;
 
-      if (status === 'deleted') {
-        if (type === 'primitive') {
-          rows += `${indentMinus(indentSize - spacesCount)}${name}: ${value}`;
-        } else {
-          const { children } = item;
-          rows += `${indentMinus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
-        }
-      }
+        case 'deleted':
+          return type === 'primitive'
+            ? `${indentMinus(indentSize - spacesCount)}${name}: ${value}`
+            : `${indentMinus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
 
-      if (status === 'updated') {
-        const { children } = item;
-        rows += `${currentIndent}${name}: ${iter(children, depth + 1)}`;
-      }
+        case 'updated':
+          return `${currentIndent}${name}: ${iter(children, depth + 1)}`;
 
-      if (status === 'added') {
-        const { previousValue } = item;
-        if (previousValue === undefined) {
-          if (type === 'primitive') {
-            rows += `${indentPlus(indentSize - spacesCount)}${name}: ${value}`;
-          } else {
-            const { children } = item;
-            rows += `${indentPlus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
+        case 'added':
+          if (previousValue === undefined) {
+            return type === 'primitive'
+              ? `${indentPlus(indentSize - spacesCount)}${name}: ${value}`
+              : `${indentPlus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
           }
-        } else { // not undefined
-          const { previousValueType } = item;
+          // not undefined
           if (type === 'json' && previousValueType === 'primitive') {
-            const { children } = item;
-            rows += `${indentMinus(indentSize - spacesCount)}${name}: ${previousValue}\n${indentPlus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
+            return `${indentMinus(indentSize - spacesCount)}${name}: ${previousValue}\n${indentPlus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}`;
           }
           if (type === 'primitive' && previousValueType === 'json') {
-            const { children } = item;
-            rows += `${indentMinus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}\n${indentPlus(indentSize - spacesCount)}${name}: ${value}`;
+            return `${indentMinus(indentSize - spacesCount)}${name}: ${iter(children, depth + 1)}\n${indentPlus(indentSize - spacesCount)}${name}: ${value}`;
           }
           if (type === 'primitive' && previousValueType === 'primitive') {
-            rows += `${indentMinus(indentSize - spacesCount)}${name}: ${previousValue}\n${indentPlus(indentSize - spacesCount)}${name}: ${value}`;
+            return `${indentMinus(indentSize - spacesCount)}${name}: ${previousValue}\n${indentPlus(indentSize - spacesCount)}${name}: ${value}`;
           }
-        }
+
+          break;
+
+        default:
+          return '';
       }
-      return rows;
+      return '';
     });
 
     return [
