61df4efc2e033362c0c90cd59e2cd45f5b42c510
Nikolai Ochirov <dubdubman@ya.ru>
2022-05-17 23:11:48 +0300

    keep linter happy, refactor gendiff to remove continue

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,11 +2,9 @@ import _ from 'lodash';
 import isObjectAndNotArray from './utils.js';
 
 const genDiff = (json1, json2) => {
-  const diff = [];
   const keys = (_.union([...Object.keys(json1), ...Object.keys(json2)])).sort();
 
-  for (let i = 0; i < keys.length; i += 1) {
-    const key = keys[i];
+  const diff = keys.map((key) => {
     const value1 = json1[key];
     const value2 = json2[key];
     const value1Type = _.isPlainObject(value1) ? 'json' : 'primitive';
@@ -14,83 +12,67 @@ const genDiff = (json1, json2) => {
 
     // если ключа нет во втором файле, значит его убрали
     if (!_.has(json2, key)) {
-      if (value1Type === 'json') {
-        const children = genDiff(value1, value1);
-        diff.push({
-          name: key, value: value1, type: value1Type, status: 'deleted', children,
-        });
-      } else {
-        diff.push({
+      return value1Type === 'json'
+        ? {
+          name: key, value: value1, type: value1Type, status: 'deleted', children: genDiff(value1, value1),
+        }
+        : {
           name: key, value: value1, type: value1Type, status: 'deleted',
-        });
-      }
-      continue;
+        };
     }
     // если ключа нет в первом файле, значит его добавили
     if (!_.has(json1, key)) {
-      if (value2Type === 'json') {
-        const children = genDiff(value2, value2);
-        diff.push({
-          name: key, value: value2, type: value2Type, status: 'added', children,
-        });
-      } else {
-        diff.push({
+      return value2Type === 'json'
+        ? {
+          name: key, value: value2, type: value2Type, status: 'added', children: genDiff(value2, value2),
+        }
+        : {
           name: key, value: value2, type: value2Type, status: 'added',
-        });
-      }
-      continue;
+        };
     }
     // значения по ключу равны
     if (_.isEqual(value1, value2)) {
-      if (value1Type === 'primitive') {
-        diff.push({
+      return value1Type === 'primitive'
+        ? {
           name: key, value: value1, type: value1Type, status: 'unchanged',
-        });
-      } else {
-        const children = genDiff(value1, value1);
-        diff.push({
-          name: key, value: value1, type: value1Type, status: 'unchanged', children,
-        });
-      }
-      continue;
+        }
+        : {
+          name: key, value: value1, type: value1Type, status: 'unchanged', children: genDiff(value1, value1),
+        };
     }
-
     // значения разные, но одновременно не вложенные json
     if ((!isObjectAndNotArray(value1) || !isObjectAndNotArray(value2))
       && !_.isEqual(value1, value2)) {
       if (value1Type === 'primitive' && value2Type === 'primitive') {
-        diff.push({
+        return {
           name: key, value: value2, type: value2Type, status: 'added', previousValue: value1, previousValueType: value1Type,
-        });
+        };
       }
       if (value2Type === 'json' && value1Type === 'primitive') {
-        const children = genDiff(value2, value2);
-        diff.push({
-          name: key, value: value2, type: value2Type, status: 'added', children, previousValue: value1, previousValueType: value1Type,
-        });
+        return {
+          name: key, value: value2, type: value2Type, status: 'added', children: genDiff(value2, value2), previousValue: value1, previousValueType: value1Type,
+        };
       }
       if (value2Type === 'primitive' && value1Type === 'json') {
-        const children = genDiff(value1, value1);
-        diff.push({
-          name: key, value: value2, type: value2Type, status: 'added', previousValue: value1, previousValueType: value1Type, children,
-        });
+        return {
+          name: key, value: value2, type: value2Type, status: 'added', previousValue: value1, previousValueType: value1Type, children: genDiff(value1, value1),
+        };
       }
     }
-
     // значения разные, вложенные json
     if (isObjectAndNotArray(value1) && isObjectAndNotArray(value2)) {
-      const children = genDiff(value1, value2);
       let status = 'added';
       if (_.has(json1, key) && _.has(json2, key)) {
         status = 'updated';
       }
-      diff.push({
-        name: key, value: 'json', type: 'json', status, children,
-      });
+      return {
+        name: key, value: 'json', type: 'json', status, children: genDiff(value1, value2),
+      };
     }
-  }
+    return {};
+  });
 
-  return _.sortBy(diff, ['name']);
+  return diff;
 };
 
 export default genDiff;
